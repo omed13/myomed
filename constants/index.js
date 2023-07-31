73b78c11d8404fb51b3dea26a72f6e653e47f7cd
omed13 <116465065+omed13@users.noreply.github.com>
2023-07-31 15:24:13 +0300

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x0b8807ea382ce77cf3aafc82db20bf038bf63416";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
